1fc349bdf380325a4634368283dd87617998acd7
MoHuzaifa <muhammad.huzaifa@tintash.com>
2022-02-23 16:38:13 +0500

    Add twitter button UI

---
+++ config-overrides.js
@@ -0,0 +1,36 @@
+const webpack = require('webpack');
+
+module.exports = function override(config, env) {
+  config.resolve.fallback = {
+    // url: false,
+    fs: false,
+    // assert: false,
+    // crypto: false,
+    // http: false,
+    // https: false,
+    // os: false,
+    // buffer: false,
+    // stream: false,
+    // path: false,
+    // zlib: false,
+    url: require.resolve('url/'),
+    // fs: require.resolve('fs'),
+    assert: require.resolve('assert'),
+    crypto: require.resolve('crypto-browserify'),
+    http: require.resolve('stream-http'),
+    https: require.resolve('https-browserify'),
+    os: require.resolve('os-browserify/browser'),
+    buffer: require.resolve('buffer'),
+    stream: require.resolve('stream-browserify'),
+    path: require.resolve('path-browserify'),
+    zlib: require.resolve('browserify-zlib'),
+  };
+  config.plugins.push(
+    new webpack.ProvidePlugin({
+      process: 'process/browser',
+      Buffer: ['buffer', 'Buffer'],
+    }),
+  );
+
+  return config;
+};
--- src/App.js
+++ src/App.js
@@ -1,23 +1,104 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
+import { Follow } from 'react-twitter-widgets';
+import axios from 'axios';
+
 import './App.css';
 
 function App() {
+  const [entries, setEntries] = useState(0);
+  const bearerToken = 'AAAAAAAAAAAAAAAAAAAAAOIPZgEAAAAAUVorPVlTYpXaoTa008FFWBW%2FbBA%3DOvFtbECGARWul48wSRdKq9eyvfima2KMTqNsa6MmcnMV0nimvl';
+
+  useEffect(() => {
+    const handleTwitter = async () => {
+      // const followersList = await axios.get('https://catfact.ninja/fact');
+
+      const followersList = await axios.get(
+        'https://api.twitter.com/2/users/1269635675684569088/followers',
+        {
+          headers: {
+            Authorization:
+              'Bearer AAAAAAAAAAAAAAAAAAAAAOIPZgEAAAAAUVorPVlTYpXaoTa008FFWBW%2FbBA%3DOvFtbECGARWul48wSRdKq9eyvfima2KMTqNsa6MmcnMV0nimvl',
+          },
+        },
+      );
+      console.log(followersList);
+    };
+
+    handleTwitter();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        background: 'rgba(0,0,0,0.8)',
+      }}
+    >
+      <div
+        style={{
+          width: '40rem',
+          height: 'auto',
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          background: 'white',
+          padding: '2rem',
+          paddingLeft: '4rem',
+          paddingRight: '4rem',
+          borderRadius: '0.75rem',
+        }}
+      >
+        <h1>Hubble Diamonds</h1>
+        <h1>Giveaway</h1>
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            marginTop: '1rem',
+            padding: '0.5rem',
+            paddingLeft: '1rem',
+            paddingRight: '1rem',
+            border: '1px solid black',
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          <h1>{entries}</h1>
+          <h4>Your Entries</h4>
+        </div>
+
+        <div
+          style={{
+            width: '100%',
+            display: 'flex',
+            justifyContent: 'space-between',
+            alignItems: 'end',
+            background: '#FAF9F6',
+            padding: '0.5rem',
+            marginTop: '2rem',
+          }}
+        >
+          <Follow
+            username="HubbleDiamonds"
+            options={{ size: 'large', showCount: false }}
+          />
+
+          <form
+            onSubmit={() => {}}
+            style={{
+              display: 'flex',
+              flexDirection: 'column',
+              justifyContent: 'center',
+              alignItems: 'start',
+            }}
+          >
+            <p>Twitter Name:</p>
+            <input type="text" placeholder="@Mo__Huzaifa" />
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
